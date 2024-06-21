b8bbe49348ef72250c2096fd97cb6541190f58f8
Vamshi ch <115910730+vamshichi@users.noreply.github.com>
2024-06-21 11:17:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors(
-     origin:["https://easypay-omega.vercel.app","http://localhost:5173"],
+     origin:["http://localhost:5173"],
      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 ));
